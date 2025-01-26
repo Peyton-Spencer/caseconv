a93a03ab85c1dcd71e8ef2abc2c4a46890632c9c
Peyton-Spencer <pas1234567@gmail.com>
2025-01-25 22:23:52 -0500

    implement cases top level

--- cases.go
+++ cases.go
@@ -1,12 +1,48 @@
 package caseconv
 
+import (
+	"github.com/peyton-spencer/caseconv/bytcase"
+	"github.com/peyton-spencer/caseconv/strcase"
+)
+
 type Case uint8
 
 const (
 	CaseSnake Case = iota
 	CaseSnakeScreaming
 	CaseCamel
-	CasePascal
 	CaseKebab
 	CaseKebabScreaming
 )
+
+func (c Case) StrCase(s string) string {
+	switch c {
+	case CaseSnake:
+		return strcase.ToSnake(s)
+	case CaseSnakeScreaming:
+		return strcase.ToScreamingSnake(s)
+	case CaseCamel:
+		return strcase.ToCamel(s)
+	case CaseKebab:
+		return strcase.ToKebab(s)
+	case CaseKebabScreaming:
+		return strcase.ToScreamingKebab(s)
+	}
+	return s
+}
+
+func (c Case) BytCase(s []byte) []byte {
+	switch c {
+	case CaseSnake:
+		return bytcase.ToSnake(s)
+	case CaseSnakeScreaming:
+		return bytcase.ToScreamingSnake(s)
+	case CaseCamel:
+		return bytcase.ToCamel(s)
+	case CaseKebab:
+		return bytcase.ToKebab(s)
+	case CaseKebabScreaming:
+		return bytcase.ToScreamingKebab(s)
+	}
+	return s
+}
